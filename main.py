0a9fd8abd4db85db7b9ce91835122ffd3340a0df
BurakErkemen <burakfurkanerkemen@gmail.com>
2024-01-08 06:50:04 +0300

    Update RealTime

--- main.py
+++ main.py
@@ -5,34 +5,45 @@ from pathlib import Path
 import os
 import cv2
 from torchvision import transforms
+import time
 
 # YOLOv5 modelini yükleme
 model = torch.hub.load('ultralytics/yolov5:v6.0', 'yolov5x', pretrained=True)
 
 def object_detection(image):
-    # Modeli kullanarak resmi işleme
     results = model(image)
 
-    # İşlenmiş resmi kaydetme
     output_path = "C:\\Users\\burak\\Desktop\\deneme3"
     results.save(output_path)
 
     return results, output_path
 
 
-def RealTime_Object_Detection(frame):
-    
-    return frame
 
 
 
 def main():
-    st.title("Realtime OBject Detection")
+    st.title("OBject Detection")
+    result = st.button("ReadlTime Object Detection")
+    if result:
+        cap = cv2.VideoCapture(0)
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            results = model(frame)
+            pred = results.xyxy[0].numpy()
+            for det in pred:
+                cv2.rectangle(frame, (int(det[0]), int(det[1])), (int(det[2]), int(det[3])), (0, 255, 0), 2)
+                cv2.putText(frame, f'{model.names[int(det[5])]}: {det[4]:.2f}', (int(det[0]), int(det[1] - 10)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
+            cv2.imshow('Real-Time YoloV5 Object Detection', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
     
-
-
-
     # Fotoğraf üzerinden nesne tespiti
     st.title("YOLOv5 Object Detection")
     uploaded_file = st.file_uploader("Bir resim yükleyin", type=["jpg", "jpeg", "png"])
